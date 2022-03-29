7b967eef4fd81df51af4a398dd510207418c4ff7
Alex Todorov <alex.todorov@gluwa.com>
2022-03-29 12:08:39 +0000

    WIP: Try using substrate-test-runtime-client
    yields a bunch of compilation errors

---
+++ runtime/src/tests.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod tests {
+//    use sc_block_builder::BlockBuilderProvider;
+//    use sp_api::ProvideRuntimeApi;
+//    use sp_consensus::BlockOrigin;
+//    use sp_runtime::generic::BlockId;
+//    use sp_state_machine::ExecutionStrategy;
+//    use substrate_test_runtime_client::{
+//        prelude::*, runtime::TestAPI, TestClientBuilder,
+//    };
+
+
+    #[test]
+    fn test_runtime_storage() {
+//        let client =
+//            TestClientBuilder::new().set_execution_strategy(ExecutionStrategy::Both).build();
+//        let runtime_api = client.runtime_api();
+//        let block_id = BlockId::Number(client.chain_info().best_number);
+//
+//        runtime_api.test_storage(&block_id).unwrap();
+    }
+
+}
